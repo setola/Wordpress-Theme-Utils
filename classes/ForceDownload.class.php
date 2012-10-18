5d5428016e2e8b0d944a13503393623a946b5016
Setola <setola@gmail.com>
2012-10-18 18:17:46 +0200

    fix ForceDownload feature

--- classes/ForceDownload.class.php
+++ classes/ForceDownload.class.php
@@ -1,42 +1,37 @@
 <?php 
 
-
+/**
+ * Force an attachment to be downloaded
+ * @author etessore
+ * @version 1.0.0
+ */
 class ForceDownload{
-	var $request_field;
+	
 	/**
 	 * Enables the force to download feature
 	 * @param bool $priv
 	 * @param bool $nopriv
 	 * @param string $request_field
 	 */
-	public function __construct(bool $priv=true, bool $nopriv=true, string $request_field='id'){
+	public function __construct($priv=true, $nopriv=true){
 		if($priv) add_action('wp_ajax_download', array(&$this, 'force_download'));
 		if($nopriv) add_action('wp_ajax_nopriv_download', array(&$this, 'force_download'));
-		$this->request_field = $request_field;
 	}
 
 	/**
 	 * AJAX callback for forcing the download of a file
 	 */
-	public function force_download(){
-		if(!$this->check_request()){
+	public static function force_download(){
+		if(!self::check_request()){
 			wp_die('Unauthorized Resource');
 		}
-		
-		$post = get_post($_GET['id']);
-		$file = get_attached_file($post->ID);
+
+		$file = get_attached_file($_REQUEST['id']);
 		if(file_exists($file)){
 			header("Cache-Control: public");
 			header("Content-Description: File Transfer");
 			header("Content-Disposition: attachment; filename= " . basename($file));
 			header("Content-Transfer-Encoding: binary");
-			header("Content-Type: ". 
-					(empty($post->post_mime_type) 
-							? "application/octet-stream;" 
-							: $post->post_mime_type
-					)
-			);
-		
 			die(file_get_contents($file));
 		} else {
 			header('HTTP/1.0 404 Not Found');
@@ -47,8 +42,8 @@ class ForceDownload{
 	 * Tests if the $_REQUEST[] field is valid
 	 * Overload this to insert more checks
 	 */
-	public function check_request(){
-		if(!is_numeric($_REQUEST[$this->request_field])){
+	public static function check_request(){
+		if(!is_numeric($_REQUEST['id'])){
 			return false;
 		}
 
@@ -62,6 +57,6 @@ class ForceDownload{
 	 * @param array|string $parms @see
 	 */
 	public function force_download_anchor($id, $label, $parms){
-		return ThemeHelpers::anchor(admin_url('admin-ajax.php').'?action=download&'.$this->request_field.'='.$id, $label, $parms);
+		return ThemeHelpers::anchor(admin_url('admin-ajax.php').'?action=download&id='.$id, $label, $parms);
 	}
 }
