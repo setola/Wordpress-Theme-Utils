4db9e35dc4d9c7b0f811c679bc67f74f6783ee28
Setola <setola@gmail.com>
2012-10-18 13:50:42 +0200

    added pages, recent post and search on 404 template

--- 404.php
+++ 404.php
@@ -1,13 +1,23 @@
 <?php wp_enqueue_style('page'); ?>
 <?php get_header(); ?>
 
-<div id="main-content" class="container">
-	<div class="grid_6 alpha">
-		<?php the_404_image(); ?>
+	<div id="main-content" class="container">
+		<div class="grid_8 alpha">
+			<?php the_404_image(); ?>
+			<?php the_widget('WP_Widget_Search'); ?>
+		</div>
+		<div class="grid_8 omega">
+			<?php the_404_escape_route(); ?>
+		</div>
 	</div>
-	<div class="grid_6 omega">
-		<?php the_404_escape_route(); ?>
+	
+	<div class="container">
+		<div class="grid_8 alpha">
+			<?php the_widget('WP_Widget_Pages'); ?>
+		</div>
+		<div class="grid_8 omega">
+			<?php the_widget('WP_Widget_Recent_Posts'); ?>
+		</div>
 	</div>
-</div>
 
 <?php get_footer(); ?>
