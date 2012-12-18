2cfe4d08f5ef298cb4cf129a3b666d798c87de3d
Setola <setola@gmail.com>
2012-12-18 17:11:43 +0100

    added single hook control

--- classes/AjaxImageGenerator.class.php
+++ classes/AjaxImageGenerator.class.php
@@ -9,6 +9,10 @@
  * 	Initial release
  */
 class AjaxImageGenerator{
+	/**
+	 * @var bool hook to WordPress only once
+	 */
+	private static $is_hooked;
 	
 	/**
 	 * Sets the admin-ajax hooks
@@ -16,6 +20,17 @@ class AjaxImageGenerator{
 	 * @param bool $nopriv is the hook available for the non-registered users?
 	 */
 	public function __construct($priv=true, $nopriv=true){
+		if(!$this::$is_hooked){
+			$this->init($priv, $nopriv);
+		}
+	}
+	
+	/**
+	 * Sets the admin-ajax hooks
+	 * @param bool $priv is the hook available for registered users?
+	 * @param bool $nopriv is the hook available for the non-registered users?
+	 */
+	private function init($priv=true, $nopriv=true){
 		if($priv){
 			add_action('wp_ajax_placeholder', array(__CLASS__, 'ajax_callback'));
 		}
