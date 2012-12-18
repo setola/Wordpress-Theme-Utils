424e2b776c613ac09b7d7f695ad312c3aa2a0628
Setola <setola@gmail.com>
2012-12-18 11:02:42 +0100

    added two parameters to the constructor: you can choose if enable for register users and not

--- classes/AjaxImageGenerator.class.php
+++ classes/AjaxImageGenerator.class.php
@@ -1,17 +1,40 @@
 <?php 
 
+/**
+ * Hooks the ImageGenerator class to admin-ajax action
+ * @author etessore
+ * @version 1.0.0
+ * 
+ * 1.0.0
+ * 	Initial release
+ */
 class AjaxImageGenerator{
-	public function __construct(){
-		add_action('wp_ajax_placeholder', array(__CLASS__, 'ajax_callback'));
-		add_action('wp_ajax_nopriv_placeholder', array(__CLASS__, 'ajax_callback'));
+	
+	/**
+	 * Sets the admin-ajax hooks
+	 * @param bool $priv is the hook available for registered users?
+	 * @param bool $nopriv is the hook available for the non-registered users?
+	 */
+	public function __construct($priv=true, $nopriv=true){
+		if($priv){
+			add_action('wp_ajax_placeholder', array(__CLASS__, 'ajax_callback'));
+		}
+		if($nopriv) {
+			add_action('wp_ajax_nopriv_placeholder', array(__CLASS__, 'ajax_callback'));
+		}
 	}
 	
+	/**
+	 * This method is called by the admin-ajax subsystem.
+	 * Prints the image. 
+	 */
 	public static function ajax_callback(){
 		$image = new ImageGenerator();
-		$image
+		echo $image
 			->set('width', intval($_REQUEST['w']))
 			->set('height', intval($_REQUEST['h']))
 			->set('bg_color', 'cccccc')
-			->set('text_color', '222222');
+			->set('text_color', '222222')
+			->get_image();
 	}
 }
