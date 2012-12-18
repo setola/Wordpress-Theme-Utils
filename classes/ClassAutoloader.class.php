ded2a6d448c6976278f06bb86d801b69cc410c95
Setola <setola@gmail.com>
2012-12-18 09:54:21 +0100

    Added support for multiple search templates

--- classes/ClassAutoloader.class.php
+++ classes/ClassAutoloader.class.php
@@ -5,11 +5,14 @@
  *
  */
 class ClassAutoloader {
-	var $loading_template;
+	const WORDPRESS_THEME_UTILS_CLASS_DIR = 'classes';
+	public $loading_template;
 	
 	public function __construct() {
-		$this->set_loading_template(dirname(__FILE__) . '/%classname%.class.php');
-		spl_autoload_register(array($this, 'loader'));
+		$this->add_loading_template(
+			WORDPRESS_THEME_UTILS_PATH.'/'.self::WORDPRESS_THEME_UTILS_CLASS_DIR.'/%classname%.class.php'
+		);
+		//spl_autoload_register(array($this, 'loader'));
 	}
 	
 	/**
@@ -17,9 +20,23 @@ class ClassAutoloader {
 	 * @param string $tpl the template
 	 * @return ClassAutoloader $this for chainability
 	 */
-	public function set_loading_template($tpl){
-		$this->loading_template = $tpl;
+	public function add_loading_template($tpl){
+		$this->loading_template[] = $tpl;
 		return $this;
+	}
+	
+	/**
+	 * Hook to the WordPress init
+	 */
+	public function hook(){
+		add_action('init', array($this, 'register_autoload'));
+	}
+	
+	/**
+	 * Register the autoload function to PHP
+	 */
+	public function register_autoload(){
+		spl_autoload_register(array($this, 'loader'));
 	}
 	
 	/**
@@ -28,12 +45,17 @@ class ClassAutoloader {
 	 * @return ClassAutoloader $this for chainability
 	 */
 	private function loader($className) {
-		$filename = str_replace(
+		foreach($this->loading_template as $tpl){
+			$filename = str_replace(
 				array('%classname%'), 
 				array($className), 
-				$this->loading_template
-		);
-		if(file_exists($filename)) include_once $filename;
+				$tpl
+			);
+			if(file_exists($filename)) {
+				include_once $filename;
+				return $this;
+			}
+		}
 		return $this;
 	}
 }
