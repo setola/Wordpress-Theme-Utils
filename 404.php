3c89101992477677a5174360eba2c64bbbf75d0d
Setola <setola@gmail.com>
2013-01-16 12:02:54 +0100

    added automatically generated image and some minor fixes on element placement

--- 404.php
+++ 404.php
@@ -2,8 +2,19 @@
 <?php get_header(); ?>
 	<div id="main-content" class="container_16">
 		<div class="grid_8 alpha">
-			<?php echo HtmlHelper::image(get_template_directory_uri().'/images/error_404.gif'); ?>
-			<?php the_widget('WP_Widget_Search'); ?>
+			<?php 
+				$image = new ImageGenerator(); 
+				$image
+					->set('width', '460')
+					->set('height', '300')
+					->set('image_text', '404')
+					->set('font_size', '150')
+					->set('text_position_x', '80')
+					->set('text_position_y', '200')
+					->set('bg_color', 'cccccc')
+					->set('text_color', '222222');
+				echo $image->get_markup();			
+			?>
 		</div>
 		<div class="grid_8 omega">
 			<?php 
@@ -11,6 +22,8 @@
 				$escape->templates->set_markup('class', 'grid_6');
 				echo $escape->get_markup();
 			 ?>
+			<hr>
+			<?php the_widget('WP_Widget_Search'); ?>
 		</div>
 	</div>
 	
