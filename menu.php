5b2249c545d77db1cf2b2e83deba87070061aedc
Setola <setola@gmail.com>
2013-01-21 09:27:35 +0100

    switched to ThemeHelpers::load_css()

--- menu.php
+++ menu.php
@@ -1,16 +1,15 @@
-<div id="main-menu" class="linear-menu">
-	<?php 
-		wp_nav_menu(
-			array(
-				'theme_location'	=>	'primary',
-				'container'			=>	'',
-				'depth'				=>	1,
-				//'walker'			=>	new RemoveSpacesWalkerNavMenu(),
-				'before'			=>	'',
-				'after'				=>	'',
-				'link_before'		=>	'',
-				'link_after'		=>	''
-			)
-		);
-	?>	
-</div>
+<?php 
+	ThemeHelpers::load_css('linear-menu');
+	wp_nav_menu(
+		array(
+			'theme_location'	=>	'primary',
+			'menu_class'		=>	'linear-menu clearfix grid_16',
+			'container'			=>	'',
+			'depth'				=>	1,
+			//'walker'			=>	new RemoveSpacesWalkerNavMenu(),
+			'before'			=>	'',
+			'after'				=>	'',
+			'link_before'		=>	'',
+			'link_after'		=>	''
+		)
+	);
