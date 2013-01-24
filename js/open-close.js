fe99de8ea32163cbb2253df1c3c23e5f30cd9fa2
Setola <setola@gmail.com>
2013-01-24 16:21:24 +0100

    do not code in italian plz :)

--- js/open-close.js
+++ js/open-close.js
@@ -1,26 +1,26 @@
-var chiuso=false;
+var closed=false;
 jQuery(document).ready(function(){
-	jQuery('.open-close').hover(	function() {
-		if (!chiuso){
+	jQuery('.open-close').hover(function() {
+		if (!closed){
 			jQuery('#front-page-text').fadeOut(200);
 			jQuery(this).addClass('open');
 		}
 	}, 
 	function() {
-		if (!chiuso){
+		if (!closed){
 			jQuery('#front-page-text').fadeIn(200);
 			jQuery(this).removeClass('open');
 		}
 	}); 
 	jQuery('.open-close').click(function () {
-		if (chiuso){
+		if (closed){
 			jQuery('#front-page-text').fadeIn(200);
 			jQuery(this).removeClass('open');
-			chiuso=!chiuso;
+			closed=!closed;
 		} else {
 			jQuery('#front-page-text').fadeOut(200);
 			jQuery(this).addClass('open');
-			chiuso=!chiuso;
+			closed=!closed;
 		}
 	});
 });
