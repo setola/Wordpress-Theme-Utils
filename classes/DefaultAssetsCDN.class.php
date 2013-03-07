a39885853ebec6ca03f693dd3cd985bbc90c67f0
Setola <setola@gmail.com>
2013-03-07 18:17:40 +0100

    added a couple of css

--- classes/DefaultAssetsCDN.class.php
+++ classes/DefaultAssetsCDN.class.php
@@ -17,6 +17,9 @@ class DefaultAssetsCDN extends DefaultAssets{
 	 */
 	public function register_standard(){
 		$this->add_js('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', null, '1.9.1', true);
+		
+		$this->add_css('reset', 'http://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css', null, '2.0', 'screen');
+		$this->add_css('grid-960', 'http://cdnjs.cloudflare.com/ajax/libs/960gs/0/960.css', null, '0.1', 'screen');
 		return $this;
 	}
 }
