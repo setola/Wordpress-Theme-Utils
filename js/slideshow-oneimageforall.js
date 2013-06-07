c917e069b473718df7616d00c80d1bbbe227df25
Setola <setola@gmail.com>
2013-06-07 16:15:21 +0200

    Fixed compatibility between MergedSlideshow and ImagePreload

--- js/slideshow-oneimageforall.js
+++ js/slideshow-oneimageforall.js
@@ -1,11 +1,36 @@
 jQuery(document).ready(function(){
-	var slideshow = jQuery('#slideshow');
-	var cycle_container = slideshow.find('.cycle');
-	cycle_container.cycle({
-		speed: 800,
-		timeout: 3000,
-		slideResize: false,
-		next: slideshow.find('.next'),
-		prev: slideshow.find('.prev')
-	});	
+	jQuery('.slideshow-container').each(function(){
+		
+		var slideshow_container = jQuery(this);
+		var slideshow = slideshow_container.find('.slideshow');
+		var controls = slideshow_container.find('.controls');
+		var cycle_container = slideshow_container.find('.cycle');
+		
+		
+		/*var slideshow = jQuery('#slideshow');
+		var cycle_container = slideshow.find('.cycle');
+		var controls = slideshow.find('.controls');*/
+		
+		cycle_container.cycle({
+			speed: 800,
+			timeout: 3000,
+			slideResize: false,
+		});
+		controls.find('.play-pause').addClass('pause').click(function(){
+			var control = jQuery(this);
+			if(control.hasClass('pause')){
+				cycle_container.cycle('pause');
+				control.removeClass('pause').addClass('play');
+			} else {
+				cycle_container.cycle('resume');
+				control.removeClass('play').addClass('pause');
+			}
+		});
+		controls.find('.next').click(function(){
+			cycle_container.cycle('next');
+		});
+		controls.find('.prev').click(function(){
+			cycle_container.cycle('prev');
+		});
+	});
 });
