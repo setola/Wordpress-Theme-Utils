79e5bbbd18bec83e56441f4146620209144b99d7
Setola <setola@gmail.com>
2013-06-20 12:20:36 +0200

    Standardized with using SubstitutionTemplate object

--- classes/ShowcaseSlider.class.php
+++ classes/ShowcaseSlider.class.php
@@ -13,6 +13,14 @@
  * @subpackage image manager
  */
 class ShowcaseSlider extends GalleryHelper{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see FeatureWithAssets::load_assets()
+	 */
+	function load_assets(){
+		ThemeHelpers::load_js('showcase');
+	}
 
 	/**
 	 * (non-PHPdoc)
@@ -23,34 +31,24 @@ class ShowcaseSlider extends GalleryHelper{
 		if(empty($this->images)){
 			return '';
 		}
-
-		ThemeHelpers::load_js('jquery.showcase');
-		ThemeHelpers::load_js('slideshow-oneimageforall');
-		// vd($this->images);
+		
 		$toret = '';
+		
+		$subs = new SubstitutionTemplate();
+		$subs->set_tpl(<<<EOF
+	<div class="showcase-slide">
+		<div class="showcase-content">
+			<div class="showcase-content-wrapper">
+				%img%
+			</div>
+		</div>
+	</div>	
+EOF
+		);		
 		foreach($this->images as $id => $image) {
-
-			$tp2 = '<div class="showcase-slide">
-					<div class="showcase-content">
-					<div class="showcase-content-wrapper">
-					<img  class="" title=":title:" alt=":alt:" src=":src:?h=:height:&zc=1" style="height: :height:px">
-					</div>
-					</div>
-					</div>';
-
-
-			$class = '';
-
-			// for big image
-			$subs1 = array(
-					':class:'		=> 	$class,
-					':src:'			=>	$this->get_image_src($id),
-					':alt:'			=>	$this->get_image_alt($id),
-					':width:'		=>	$this->get_image_width($id),
-					':height:'	=>	$this->get_image_height($id),
-					':caption:'	=> 	$this->get_image_caption($id)
-			);
-			$toret .= str_replace(array_keys($subs1), array_values($subs1), $tp2);
+			$toret .= $subs
+				->set_markup('img', wp_get_attachment_image($this->get_image_id($id), $this->media_dimension))
+				->replace_markup();
 		}
 		
 		
